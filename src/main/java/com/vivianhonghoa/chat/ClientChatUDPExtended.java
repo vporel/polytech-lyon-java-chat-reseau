26cd718c98bbdedd71a1f2205678426d5f81aeeb
Hong Hoa Nguyen <hong-hoa.nguyen@etu.umontpellier.fr>
2026-03-30 11:49:19 +0200

    implement client
    # Conflicts:
    #	src/main/java/com/vivianhonghoa/chat/client/ClientChatUDP.java

--- src/main/java/com/vivianhonghoa/chat/ClientChatUDPExtended.java
+++ src/main/java/com/vivianhonghoa/chat/ClientChatUDPExtended.java
@@ -3,29 +3,10 @@ package com.vivianhonghoa.chat;
 import com.vivianhonghoa.chat.client.ClientChatUDP;
 
 import java.io.IOException;
-import java.net.SocketException;
-import java.util.Scanner;
 
 public class ClientChatUDPExtended extends ClientChatUDP {
 
-    public ClientChatUDPExtended(String pseudo, String serverAddress) throws SocketException {
+    public ClientChatUDPExtended(String pseudo, String serverAddress) throws IOException {
         super(pseudo, serverAddress);
-    }
-
-    public void listenMessage(){
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Message to send ('exit' to quit): ");
-            String message = scanner.nextLine();
-            try {
-                sendMessage(message);
-                if(message.equalsIgnoreCase("exit")) {
-                    System.out.println("Client exiting...");
-                    break;
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 }
